ab179e6ee86816567ece613df2ec293044939bbd
22MH1A42H7 <121275711+22MH1A42H7@users.noreply.github.com>
2023-06-08 00:43:50 +0530

    n = int(input()) on,tw,th,fo,fi,si,se,ei,ni=0,0,0,0,0,0,0,0,0 while n:     if n%10==1:         on+=1     elif n%10==2:         tw+=1     elif n%10==3:         th+=1     elif n%10==4:         fo+=1     elif n%10==5:         fi+=1     elif n%10==6:         si+=1     elif n%10==7:         se+=1     elif n%10==8:         ei+=1     elif n%10==9:         ni+=1     n//=10 if on<2 and tw<2 and th<2 and fo<2 and fi<2 and si<2 and se<2 and ei<2 and ni<2:     print("Unique Number") else:     print("Not Unique Number")

--- Unique_number.py
+++ Unique_number.py
@@ -1,16 +1,26 @@
-c=0
-st=input()
-for i in range(0,len(st)):
-    c=0
-    for j in range(0,len(st)):
-        if(i!=j):
-            if(st[i]==st[j]):
-                c=c+1
-            if(c==1):
-                break
-    if(c==1):
-        break
-if(c==0):
-    print('Unique Number')
+n = int(input())
+on,tw,th,fo,fi,si,se,ei,ni=0,0,0,0,0,0,0,0,0
+while n:
+    if n%10==1:
+        on+=1
+    elif n%10==2:
+        tw+=1
+    elif n%10==3:
+        th+=1
+    elif n%10==4:
+        fo+=1
+    elif n%10==5:
+        fi+=1
+    elif n%10==6:
+        si+=1
+    elif n%10==7:
+        se+=1
+    elif n%10==8:
+        ei+=1
+    elif n%10==9:
+        ni+=1
+    n//=10
+if on<2 and tw<2 and th<2 and fo<2 and fi<2 and si<2 and se<2 and ei<2 and ni<2:
+    print("Unique Number")
 else:
-    print('Not Unique Number')
+    print("Not Unique Number")
